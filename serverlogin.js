c8e11047f6b21418eaf41c4246b210ec83cb5a97
thelokeshgoel00 <40207268+thelokeshgoel00@users.noreply.github.com>
2018-10-15 18:51:15 +0530

    Add files via upload

---
+++ serverlogin.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+const PORT=8081; 
+
+fs.readFile('./loginform.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+var http = require('http');
+var fs = require('fs');
+
+const port1=8082; 
+
+fs.readFile('./project1.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port1);
+});
